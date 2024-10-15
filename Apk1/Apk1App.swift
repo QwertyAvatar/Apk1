f4c6273ec1e16356e32195857adc973affe852a3
user <user@account.com>
2024-10-15 10:27:25 +0200

    Initial Commit

---
+++ Apk1/Apk1App.swift
@@ -0,0 +1,17 @@
+//
+//  Apk1App.swift
+//  Apk1
+//
+//  Created by student on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apk1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
